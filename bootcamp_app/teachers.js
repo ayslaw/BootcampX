96ab895abfc09d28274a227a0dfa8a606ca746e1
ayslaw <ayoubf.chp@gmail.com>
2022-02-10 13:02:16 -0500

    convert to paramtized queries and protection against SQL injection

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+const queryString =`
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1 || 'JUL02'}'
+  ORDER BY teacher;
+  `;
+
+pool.query(queryString, values)
+
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
